4961bd889d9a6a7f5db47c1b9edb304a6f91fb25
laubata2000 <laubacho2000@gmail.com>
2024-06-04 17:38:46 +0700

    them index vao web

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day la branch cua gio hang</title>
+</head>
+
+<body>
+    <h1>Đây là branch của giỏ hàng</h1>
+</body>
+
+</html>
