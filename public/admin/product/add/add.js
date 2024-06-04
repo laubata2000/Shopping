a43e3268953b2a54a6e6587dc8fdb685ae16f15e
laubata2000 <laubacho2000@gmail.com>
2024-06-04 21:24:05 +0700

    suafileadd.js

--- public/admin/product/add/add.js
+++ public/admin/product/add/add.js
@@ -10,16 +10,16 @@ $(".select2_init").select2({
 var editor_config = {
     path_absolute: "/",
     selector: "textarea.my-editor",
+    relative_urls: false,
     plugins: [
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor colorpicker textpattern",
+        "insertdatetime media nonbreaking save table directionality",
+        "emoticons template paste textpattern",
     ],
     toolbar:
         "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-    relative_urls: false,
-    file_browser_callback: function (field_name, url, type, win) {
+    file_picker_callback: function (callback, value, meta) {
         var x =
             window.innerWidth ||
             document.documentElement.clientWidth ||
@@ -31,21 +31,24 @@ var editor_config = {
 
         var cmsURL =
             editor_config.path_absolute +
-            "laravel-filemanager?field_name=" +
-            field_name;
-        if (type == "image") {
+            "filemanager?editor=" +
+            meta.fieldname;
+        if (meta.filetype == "image") {
             cmsURL = cmsURL + "&type=Images";
         } else {
             cmsURL = cmsURL + "&type=Files";
         }
 
-        tinyMCE.activeEditor.windowManager.open({
-            file: cmsURL,
+        tinyMCE.activeEditor.windowManager.openUrl({
+            url: cmsURL,
             title: "Filemanager",
             width: x * 0.8,
             height: y * 0.8,
             resizable: "yes",
             close_previous: "no",
+            onMessage: (api, message) => {
+                callback(message.content);
+            },
         });
     },
 };
